b194174f3fd925c5a31a041aff75b09917bbcf6a
AlexSansI <128729558+AlexSansI@users.noreply.github.com>
2023-03-23 17:35:43 +0100

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE news (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  date DATE NOT NULL,
+  PRIMARY KEY (id)
+  INSERT INTO news (title, description, date)
+VALUES ('Nouvelle actualité', 'Description de la nouvelle actualité', '2023-03-23');
+
+);
